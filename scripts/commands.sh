1f4708372ab7e83e3b8b96bf5b41b7bde9585eb3
MohamedSAIFI0 <saifimsc@gmail.com>
2025-10-15 19:09:08 +0100

    Technique architecture added

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -1,6 +1,13 @@
+#!/bin/bash
+set -e
+LOG_FILE="/home/saifi/Desktop/Data_Projects/eccomerce_project/log/log.txt"
+
+
 python3 -m venv venv 
 source venv/bin/activate
-
 mkdir data
 mkdir scripts && touch from_local_to_s3.py generate_fake_data.py
 mkdir sql_code && touch bronze.sql gold.sql silver.sql snowpipe.sql
+pip install -r requirements.txt
+
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] Project setup completed successfully!" >> $LOG_FILE
